73444dbaf68d777afc1ab5edb76bef86a13ec245
Pawanpaa <70006485+Pawanpaa@users.noreply.github.com>
2021-06-24 20:55:40 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,40 @@
+import tkinter as tk
+import requests
+import time
+
+def getWeather(canvas):
+    city = textfield.get()
+    api = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=bd3bad1c5838d95c33839bf801c307dc"
+    json_data = requests.get(api).json()
+    condition = json_data['weather'][0]['main']
+    temp = int(json_data['main']['temp'] - 273.15)
+    min_temp = int(json_data['main']['temp'] - 273.15)
+    max_temp = int(json_data['main']['temp'] - 273.15)
+    pressure = json_data['main']['pressure']
+    humidity = json_data['main']['humidity']
+    wind = json_data['wind']['speed']
+
+    final_info = condition + "\n" + str(temp) + "C"
+    final_data = "\n" + "Max Temp: " + str(max_temp) + "\n" + "Min Temp: " + str(min_temp) + "\n" + "Pressure: " + str(pressure) + "\n" + "Humidity: " + str(humidity) + "\n" + "Wind Speed: " + str(wind) +"\n"
+    label1.config(text = final_info)
+    label2.config(text = final_data)
+
+canvas = tk.Tk()
+canvas.geometry("600x500")
+canvas.title("Weather App")
+
+f = ("poppins", 15, "bold")
+t = ("poppins", 35, "bold")
+
+textfield = tk.Entry(canvas, font = t)
+textfield.pack(pady = 20)
+textfield.focus()
+textfield.bind('<Return>', getWeather)
+
+
+label1 = tk.Label(canvas, font = t)
+label1.pack()
+label2 = tk.Label(canvas, font = f)
+label2.pack()
+
+canvas.mainloop()
